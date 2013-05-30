b6edd6aac20bb1556f143d926571a0674cf3de06
s_oono <s_oono@rjc-73f0ce65b59.r-jc.local>
2013-05-30 17:56:58 +0900

    test22

--- PythonTest/src/Moji.py
+++ PythonTest/src/Moji.py
@@ -3,5 +3,14 @@ Created on 2013/05/30
 
 @author: s_oono
 '''
-VALUE = r"TEST \nTEST"
+VALUE = "TEST \nTEST"
 print(VALUE)
+
+VALUE = "TEST2 \nTEST2"
+print(VALUE)
+
+print(VALUE[2])
+
+VALUE[2] = "W"
+
+print(VALUE[2])
